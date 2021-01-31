3fa4d18ec0391e2388ee47b9e89985cc99c2a9ff
diego14008 <skypemen12@gmail.com>
2021-01-31 13:41:58 -0500

    Fin seccion 18

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -7,7 +7,7 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuariosController');
-const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarJWT,validarADMIN_ROLE,validarADMIN_ROLE_o_MismoUsuario } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -28,6 +28,7 @@ router.put(
   "/:id",
   [
     validarJWT,
+    validarADMIN_ROLE_o_MismoUsuario,
     check("nombre", "El nombre es obligatorio.").not().isEmpty(),
     check("email", "El email es obligatorio.").isEmail(),
     check("role", "El role es obligatorio.").not().isEmpty(),
@@ -36,7 +37,7 @@ router.put(
   actualizarUsuario
 );
 
-router.delete("/:id",validarJWT, borrarUsuario);
+router.delete("/:id",[validarJWT,validarADMIN_ROLE], borrarUsuario);
 
 /**exportamos el router */
 module.exports = router;
