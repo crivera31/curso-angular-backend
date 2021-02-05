7fbb1011675f59410d57f38ffbc834608475a60f
diego14008 <skypemen12@gmail.com>
2021-02-04 22:49:27 -0500

    agregando ruta test

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();/*importanto variable de entornos */
 const morgan = require('morgan');
-
+const publicIp = require('public-ip');
+const ip2loc = require("ip2location-nodejs");
 const express = require('express') /**importando express */
 const cors = require('cors')
+
+ip2loc.IP2Location_init("./IP2LOCATION-LITE-DB1.BIN");
 
 const { dbConnection } = require('./database/config');
 
@@ -33,6 +36,17 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 
 app.use('/api/uploads', require('./routes/upload'));
+
+app.get('/test', (req, res) =>{
+	(async () => {
+        const ip = await publicIp.v4()
+        return res.send({
+            myIp: ip,
+            country_short: ip2loc.IP2Location_get_all(ip).country_short,
+            country_long: ip2loc.IP2Location_get_all(ip).country_long,
+        })
+    })();
+});
 
 /*iniciar el servidor*/
 /**port: 3000 */
